054d054c1fc87a1270de07259db25b2664c58cc4
Zedrichu <adrzvizdencojr@gmail.com>
2023-07-04 01:00:45 +0200

    Type ahead search box implemented

---
+++ 06 - Type Ahead Search/script.js
@@ -0,0 +1,43 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint) // Returns a promise 
+    .then(blob => blob.json()) // Returns another promise 
+    .then(data => cities.push(...data)); // Spread into the data
+
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        // figure out if the city or state matches the search
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+// Pre-made function on the internet
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
