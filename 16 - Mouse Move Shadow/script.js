0125e494c7fe471d66ae03d89319ba412a0217c8
Zedrichu <adrzvizdencojr@gmail.com>
2023-07-07 00:39:25 +0200

    Shadows

---
+++ 16 - Mouse Move Shadow/script.js
@@ -0,0 +1,28 @@
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+
+function shadow(e) {
+    // const width = hero.offsetWidth;
+    // const height = hero.offsetHeight;
+    const { offsetWidth: width, offsetHeight: height } = hero; // ES6 destructuring
+    let { offsetX: x, offsetY: y } = e;
+
+    const walk = 300; //300px
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / width * walk) - (walk / 2));
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+                             ${-xWalk}px ${yWalk}px 0 rgba(0, 255, 255, 0.7),
+                             ${yWalk}px ${-xWalk}px 0 rgba(0, 255, 0, 0.7),
+                             ${-yWalk}px ${xWalk}px 0 rgba(0, 0, 255, 0.7)`;
+
+    console.log(xWalk, yWalk)
+}
+
+hero.addEventListener('mousemove', shadow);
